13ca14c4428410490d2400e9bfc2ee70890db341
Daniel Zabala <danielzabalanava@gmail.com>
2025-01-15 11:14:08 -0400

    sign in screen refactored, created parent viewmodel for sharing

---
+++ lib/auth/accloss/src/commonMain/kotlin/org/closs/auth/presentation/viewmodel/DefaultSignInViewModel.kt
@@ -0,0 +1,110 @@
+package org.closs.auth.presentation.viewmodel
+
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavOptions
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.closs.auth.shared.data.repository.AuthRepository
+import org.closs.auth.shared.presentation.events.SignInEvents
+import org.closs.auth.shared.presentation.viewmodel.SignInViewModel
+import org.closs.core.presentation.shared.messages.Messages
+import org.closs.core.presentation.shared.navigation.Destination
+import org.closs.core.presentation.shared.navigation.Navigator
+import org.closs.core.types.shared.auth.dto.SignInDto
+import org.closs.core.types.shared.state.RequestState
+
+class DefaultSignInViewModel(
+    override val navigator: Navigator,
+    override val messages: Messages,
+    override val authRepository: AuthRepository,
+) : SignInViewModel(
+    navigator = navigator,
+    messages = messages,
+    authRepository = authRepository
+) {
+    override val state = _state.asStateFlow()
+
+    override fun onEvent(event: SignInEvents) {
+        when (event) {
+            is SignInEvents.OnCompanyChanged -> companyChanged(event.value)
+            SignInEvents.OnCompanySubmitted -> companySubmit()
+            is SignInEvents.OnUsernameChanged -> signInUsernameChanged(event.value)
+            is SignInEvents.OnPasswordChanged -> signInPasswordChanged(event.value)
+            SignInEvents.TogglePasswordVisibility -> togglePasswordVisibility()
+            SignInEvents.OnSignInSubmit -> signInSubmit()
+            SignInEvents.OnNavigateToForgotPassword -> navigateForgotPassword()
+            SignInEvents.OnNavigateToAccounts -> navigateToAccounts()
+        }
+    }
+
+    // todo: company search
+    private fun companySubmit() {
+        viewModelScope.launch {
+        }
+    }
+
+    private fun navigateToAccounts() {
+        resetState()
+        viewModelScope.launch {
+            navigator.navigate(
+                destination = Destination.Accounts,
+                navOptions = NavOptions.Builder().apply {
+                    setPopUpTo(route = Destination.SignIn, inclusive = false)
+                    setLaunchSingleTop(true)
+                }.build()
+            )
+        }
+    }
+
+    // todo: save session
+    override fun signInSubmit() {
+        if (onSignInError()) return
+        viewModelScope.launch {
+            val call = authRepository.signIn(
+                signInDto = SignInDto(
+                    username = _state.value.username,
+                    password = _state.value.password
+                )
+            )
+            when (call) {
+                is RequestState.Error -> {
+                    _state.update { state ->
+                        state.copy(
+                            isLoading = false,
+                            errorMessage = call.error.message
+                        )
+                    }
+                    messages.sendMessage(call.error)
+                }
+                is RequestState.Success -> {
+                    _state.update { state ->
+                        state.copy(
+                            isLoading = false,
+                            errorMessage = null
+                        )
+                    }
+
+                    messages.sendMessage(call.data)
+                    navigator.navigate(
+                        destination = Destination.Home,
+                        navOptions = NavOptions.Builder().apply {
+                            setPopUpTo(route = Destination.AuthGraph, inclusive = true)
+                            setLaunchSingleTop(true)
+                        }.build()
+                    )
+
+                    resetState()
+                }
+                else -> {
+                    _state.update { state ->
+                        state.copy(
+                            isLoading = true,
+                            errorMessage = null
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
